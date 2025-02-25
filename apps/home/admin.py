8e8ff5a64cba0a2067aa53edd631f3987b4992b1
dci-zoje <zoje.gabili@dci-student.org>
2025-02-25 11:58:41 +0100

    Adding a custom user

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,11 +4,18 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-from .models import Subject, Category, Quiz, Question, Answer
+from .models import Subject, Category, Quiz, Question, Answer, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Register your models here.
-
 admin.site.register(Subject)
 admin.site.register(Category)
 admin.site.register(Quiz)
--- apps/home/models.py
+++ apps/home/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=255, unique=True)
